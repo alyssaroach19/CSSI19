f2556f6265ccaa4662da96ca58af2c994fbecbe9
alyssaroach <alyssaroach@cs-c09-5.mec.science>
2019-07-29 11:59:45 -0400

    script2

--- day5/script.js
+++ day5/script.js
@@ -8,3 +8,24 @@ console.log("7");
 console.log("8");
 console.log("9");
 console.log("10");
+
+//prompt
+/*
+let result = prompt("Enter your name");
+console.log("Hello", result);
+
+let num = prompt("Enter a number");
+num = number(num);
+
+if(isNaN(num)){
+    num = 10;
+}
+
+num = num + 10;
+console.log(num);
+*/
+let num = prompt("Enter a grade");
+        if (num >= 70)
+        {console.log("You've passed");}
+    else if(num < 70)
+    {console.log("You've failed");}
---
+++ day5/script2.js
@@ -0,0 +1,44 @@
+/*
+function f(x)
+{
+    return 3 * x + 7;
+}
+console.log(f(2));
+console.log(f(12));
+console.log(f(7));  
+
+
+function g (x)
+{
+    return 5 * x + 5;
+}
+console.log(g(4));
+console.log(g(10));
+console.log(g(5));
+console.log(g(3));
+
+function h (x)
+{
+    return 5 *x ** 2 - 4 * x + 2;
+}
+console.log(g(4));
+console.log(g(10));
+console.log(g(5));
+console.log(g(3));
+
+function  i (x,y)
+{
+    return x ** 2 + y ** 2;
+}
+console.log(i(4,5));
+console.log(i(5,7));
+console.log(i(7,10));
+*/
+
+function j (x,y,z)
+{
+    return x + y + z;
+}
+console.log(j(4,5,6));
+console.log(j(7,4,3));
+console.log(j(10,6,2));
