9d28571fab74548fab7021504fcc52c6c1e41cb3
alyssaroach <alyssaroach@cs-c09-16.mec.science>
2019-08-05 16:54:07 -0400

    day9

---
+++ day7/practice.py
@@ -0,0 +1,29 @@
+def Greeting(name):
+    print "Hello," ,name
+
+def echo():
+    response = raw_input("Enter a string?")
+    print response + "\n" + response 
+
+
+def fizzbuzz(num)
+{
+    ifnum % 3 == 0 and num % 5 == 0
+    {
+        print "fizzbuzz";
+    }
+    else if num % 3 == 0
+    {
+        print "fizz"
+    }
+    else if(num % 5 == 0)
+        print "buzz"
+    else
+    {
+        print num
+    }
+}
+fizzbuzz(9)
+fizzbuzz(10)
+fizzbuzz(30)
+fizzbuzz(7)
